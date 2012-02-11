ece5597ead12c33039f09e153887cab2a819c7ba
Afzal <afzal@MeanMech>
2012-02-11 17:30:13 -0500

    include thread.h and vector.h

--- code/threads/alarm.h
+++ code/threads/alarm.h
@@ -22,6 +22,8 @@
 #include "callback.h"
 #include "timer.h"
 #include "vector.h"
+#include "thread.h"
+#include "interrupt.h"
 
 // The following class defines a software alarm clock. 
 class Alarm : public CallBackObj {
