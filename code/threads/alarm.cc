1bf3bdbdbecacc8c614ee7d5efef16ba8a0dc837
Afzal <afzal@MeanMech>
2012-02-11 17:35:41 -0500

    ..

--- code/threads/alarm.cc
+++ code/threads/alarm.cc
@@ -66,7 +66,7 @@ Alarm::GoToSleepFor(int howLong)
 {
 	Threadstruct temp;
 	temp.thread1 = kernel->currentThread;
-	temp.interrupt1->Schedule(Scheduler->ReadyToRun(temp.thread1), howLong, TimerInt);
+	kernel->interrupt->Schedule(Scheduler->ReadyToRun(temp.thread1), howLong, TimerInt);
 	interruptedthreads.push_back(temp);
-	currentThread->Sleep();
+	kernel->currentThread->Sleep();
 }
