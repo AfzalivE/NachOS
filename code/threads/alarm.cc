b3b2e9f258488cf1fe1830a667f06997b02859d5
Afzal <afzal@MeanMech>
2012-02-11 19:31:14 -0500

    .

--- code/threads/alarm.cc
+++ code/threads/alarm.cc
@@ -79,7 +79,7 @@ Alarm::GoToSleepFor(int howLong)
     }
 
     int i, j;
-    DEBUG("myBug", "----------making temp thread");
+    printf("%s\n", "----------making temp thread");
     Threadstruct newValue;
 
     for (i = 1; i < interruptedthreads.size(); i++) {
