948d4c489a6417d16f55f22db74662bf2adeb8a0
Afzal <afzal@MeanMech>
2012-02-12 15:55:23 -0500

    .

--- code/threads/alarm.cc
+++ code/threads/alarm.cc
@@ -21,16 +21,6 @@
 //		occur at random, instead of fixed, intervals.
 //----------------------------------------------------------------------
 
-static int Alarm::Compare(Threadstruct x, Threadstruct y) {
-    if (x.time < y.time) {
-        return -1;
-    } else if (x.time == y.time) {
-        return 0;
-    } else {
-        return 1;
-    }
-}
-
 Alarm::Alarm(bool doRandom)
 {
     timer = new Timer(doRandom, this);
