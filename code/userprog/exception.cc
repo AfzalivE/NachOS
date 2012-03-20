5318cd5b3ffceb9201dad3c5d1cd587374c32526
Afzal <afzal@MeanMech>
2012-03-19 22:40:08 -0400

    test?

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -71,7 +71,7 @@ ExceptionHandler(ExceptionType which)
 		int virtAddr = kernel->machine->ReadRegister(39);
 		unsigned int vpn = (unsigned) virtAddr/PageSize;
 		for(int i=0; i < NumPhysPages; i++) 		{
-		if(ipt[i]->vPage == vpn && ipt[i]->Process_Id == kernel->currentThread->space)
+		if(ipt[i].vPage == vpn && ipt[i].Process_Id == kernel->currentThread->space)
 		{
 		IntStatus oldlevel = kernel->interrupt->SetLevel(IntOff); //interrupt disable
 		kernel->machine->tlb[whichTLBPage].virtualPage = ipt[i].vPage;
